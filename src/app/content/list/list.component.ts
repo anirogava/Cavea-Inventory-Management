149a4d12fcd3ec53d41484b0dd30cb20d6a7bf3c
Ani Rogava <anarogava96@gmail.com>
2022-03-06 19:29:51 +0400

    small changes

--- src/app/content/list/list.component.ts
+++ src/app/content/list/list.component.ts
@@ -19,8 +19,6 @@ export class ListComponent implements OnInit {
   get emptySting(): string {
     return EMPTY;
   }
-
- 
   constructor(private fireService: FireStoreService) {}
 
   getAlldata() {
